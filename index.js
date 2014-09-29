aed961a59f0bbed64c32239e733927f8596ab515
cb <cb050034@gmail.com>
2014-09-29 14:50:49 +0800

    use serve-static

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Setup basic express server
 var path = require('path');
+var serveStatic = require('serve-static');
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -7,10 +8,7 @@ var io = require('socket.io')(server);
 var port = process.env.PORT || 3331;
 var distributor = require("./distributor.js")(io);
 
-app.configure(function () {
-	// Routing
-	app.use(express.static(path.join(__dirname, 'public')));
-});
+app.use(serveStatic('public'));
 
 app.get("/", function(req, res){
 	res.sendfile("public/index.html");
@@ -18,32 +16,4 @@ app.get("/", function(req, res){
 
 server.listen(port, function () {
 	console.log('Server listening at port %d', port);
-});
-
-var test = io.of("/test");
-
-test.on("connection", function(socket){
-	
-	socket.join("asf");
-	
-	socket.on("init", function(){
-		socket.emit("new_direct_msg", {
-			uid: "2793773712",
-			name: "崔斌1",
-			count: 2
-		});
-	});
-	socket.on("clear_direct_msg", function(){
-		console.log("clear_direct_msg");
-	});
-	
-	setInterval(function(){
-		socket.emit("new_direct_msg", {
-			uid: "2793773712",
-			name: "崔斌1",
-			count: 2
-		});
-	}, 5000);
-	
-	socket.disconnect();
 });
